5d4291c3af16d94f81cddcb5a0873d5e919d40ca
imoteriannah <imoter.iannah@student.moringaschool.com>
2023-08-27 09:37:43 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+const findMatching = (arr, name) =>
+  arr.filter((driver) => driver.toLowerCase() === name.toLowerCase());
+
+const fuzzyMatch = (arr, name) =>
+  arr.filter(
+    (driver) => driver.toLowerCase().indexOf(name.toLowerCase()) === 0
+  );
+
+const matchName = (arr, name) => arr.filter((driver) => driver.name === name);
